a880eb993ab6fed17f15114d66710e72e35f073d
Bruno <brossani23@gmail.com>
2024-05-21 21:59:54 -0300

    Product section frontend finished

--- resources/views/livewire/categories/show-category.blade.php
+++ resources/views/livewire/categories/show-category.blade.php
@@ -1,68 +1,48 @@
 <div class="flex flex-wrap p-10">
-    @foreach ($products as $product)
-        <div wire:key="{{ $product->id }}" class="flex flex-col w-full p-2 mb-4 md:w-1/2 lg:w-1/3 xl:w-1/4">
-            <div class="flex flex-col h-full overflow-hidden bg-white rounded-lg md:shadow-md">
-                <div class="flex flex-col h-full p-6">
+    <ul class="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        @foreach ($products as $product)
+            <li wire:key="{{ $product->id }}" class="flex flex-col mb-4">
+                <div class="flex flex-col h-full overflow-hidden bg-white rounded-lg shadow-md">
                     <div class="flex items-center justify-center">
-                        <a wire:navigate href="{{route('products.show', ['product' => $product->id])}}">
-                            <img class="object-contain w-full h-48" loading="lazy" src="{{ asset('storage/img/products/' . $product->image ) }}" alt="{{ $product->name }}">
+                        <a wire:navigate href="{{ route('products.show', ['product' => $product->id]) }}">
+                            <img class="object-contain w-full h-48 pt-5" loading="lazy" src="{{ asset('storage/img/products/' . $product->image ) }}" alt="{{ $product->name }}">
                         </a>
                     </div>
-                    <div class="flex flex-col flex-grow">
-                        <p class="mt-2 text-2xl font-bold text-green-500">${{ $product->price }}</p>
-                        <a wire:navigate href="{{route('products.show', ['product' => $product->id])}}">
+                    <div class="flex flex-col flex-grow px-6 py-2">
+                        <p class="text-2xl font-normal text-green-500">${{ $product->price }}</p>
+                        <a wire:navigate href="{{ route('products.show', ['product' => $product->id]) }}">
                             <h2 class="mt-2 text-xl font-semibold text-gray-800">{{ $product->name }}</h2>
                         </a>
                     </div>
-
-                    <div class="mt-2">
-                        @guest
-                            <div class="flex flex-row">
-                                <livewire:counter 
-                                    :productId="$product->id"
-                                />
-                                
-                                <button type="button" wire:click="$dispatch('showAddToCart', {{$product->id}})" class="ml-auto md:w-auto">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                    
+                    @if(auth()->check() && auth()->user()->admin === 1)
+                        <div class="my-2 mt-auto">
+                            <div class="flex flex-row mt-5">
+                                <a wire:navigate href="{{route('products.edit', ['product' => $product->id])}}" class="mr-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                    </svg>
+                                </a>
+                                <button type="button" wire:click="$dispatch('showAlert', {{$product->id}})">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
                                 </button>
                             </div>
-                        @endguest
-
-                        @auth
-                            @if(auth()->user()->admin === 1)
-                                <div class="flex flex-row mt-5">
-                                    <a wire:navigate href="{{route('products.edit', ['product' => $product->id])}}" class="mr-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                                        </svg>
-                                    </a>
-                                    <button type="button" wire:click="$dispatch('showAlert', {{$product->id}})">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                        </svg>       
-                                    </button>
-                                </div>
-                            @else
-                                <div class="flex flex-row">
-                                    <livewire:counter 
-                                        :productId="$product->id"
-                                    />
-                                    
-                                    <button type="button" wire:click="$dispatch('showAddToCart', {{$product->id}})" class="ml-auto md:w-auto">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            @endif
-                        @endauth
-                    </div>
+                        </div>
+                    @else
+                        <div class="px-6 py-2 mt-auto">
+                            @livewire('cart.add-to-cart', ['productId' => $product->id])
+                        </div>
+                    @endif
                 </div>
-            </div>
-        </div>
-    @endforeach
+            </li>
+        @endforeach
+    </ul>
 </div>
 
 @push('scripts')
