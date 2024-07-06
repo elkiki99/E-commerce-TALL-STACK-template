e891a6c307c56811b6fb24fb16814e3c6263ab75
Bruno <brossani23@gmail.com>
2024-07-05 21:00:13 -0300

    alpine modals finished

--- resources/views/livewire/tags/show-tag.blade.php
+++ resources/views/livewire/tags/show-tag.blade.php
@@ -49,7 +49,9 @@
                                 <livewire:likes.add-to-likes :product="$product" />
                             @endif
                             <a wire:navigate href="{{ route('products.show', ['product' => $product->id]) }}">
-                                <img class="object-cover w-64 h-64 transition-transform duration-200 transform hover:scale-105" loading="lazy" src="{{ asset('storage/img/products/' . $product->image ) }}" alt="{{ $product->name }}">
+                                <img class="object-cover w-64 h-64 transition-transform duration-200 transform hover:scale-105" loading="lazy" 
+                                {{-- src="{{ asset('storage/img/products/' . $product->image ) }}"  --}}
+                                alt="{{ $product->name }}">
                             </a>
                         </div>
                         <div class="mx-5">
@@ -76,13 +78,36 @@
                                         </a>
                                     </div>
                                         <div class="flex items-center">
-                                        <button type="button" class="mr-2" wire:click="$dispatch('showAlert', {{$product->id}})">
+                                        <button type="button" class="mr-2" x-on:click.prevent="$dispatch('open-modal', 'delete-product-{{ $product->id }}')">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                                 stroke="currentColor" class="w-6 h-6 text-blue-600 dark:text-blue-400">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                             </svg>
                                         </button>   
+
+                                        <x-modal name="delete-product-{{ $product->id }}" :show="$errors->isNotEmpty()" focusable>
+                                            <form wire:submit="deleteProduct({{ $product->id }})" class="p-6">
+                                    
+                                                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                    {{ __('Delete product?') }}
+                                                </h2>
+                                    
+                                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                                    {{ __('Are you sure you want to delete this product?') }}
+                                                </p>
+                                    
+                                                <div class="flex justify-end mt-6">
+                                                    <x-secondary-button x-on:click="$dispatch('close')">
+                                                        {{ __('Cancel') }}
+                                                    </x-secondary-button>
+                                    
+                                                    <x-danger-button class="ms-3">
+                                                        {{ __('Delete product') }}
+                                                    </x-danger-button>
+                                                </div>
+                                            </form>
+                                        </x-modal>
                                     </div>
                                 </div>
                             </div>
