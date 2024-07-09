eab00257979582bbf472543892262bccfa832a57
Bruno <brossani23@gmail.com>
2024-07-09 00:30:29 -0300

    starting product star ranking functionalty

---
+++ app/Livewire/Products/ProductRanking.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire\Products;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class ProductRanking extends Component
+{
+    public $product;
+    public $averageRating;
+
+    public function mount($product)
+    {
+        $this->product = $product;
+        $this->calculateAverageRating();
+    }
+
+    public function calculateAverageRating()
+    {
+        $this->product = Product::find($this->product);
+        $this->averageRating = 
+            // $product->ratings()->avg('rating')
+            3.5
+        ;
+    }
+
+    public function render()
+    {
+        return view('livewire.products.product-ranking');
+    }
+}
